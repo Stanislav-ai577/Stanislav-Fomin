4e76a6fdb72adaf91e0ebc3ddf24d7e13eec6fd9
Stanislav-ai577 <stanislav.fomin98@gmail.com>
2023-11-21 19:41:35 +0300

    Lesson 9 complete

---
+++ Lesson9/Assets/Source/Scripts/CannonAction/PlayerShoot.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    [SerializeField] private CannonLvl1 _currentCannon;
+
+    [SerializeField] private CannonLvl1 _cannonLvl1;
+    [SerializeField] private CannonLvl2 _cannonLvl2;
+    [SerializeField] private CannonLvl3 _cannonLvl3;
+ 
+    private void Update()
+    {
+         if (Input.GetKeyDown(KeyCode.Space) && _currentCannon.CanShoot && _currentCannon.Ammo > 0)
+         {
+            _currentCannon.Shoot();
+         }
+         if(Input.GetKey(KeyCode.R))
+         {
+            _currentCannon.Reload();    
+         }
+         if (Input.GetKey(KeyCode.Alpha1))
+         {
+            _cannonLvl1.gameObject.SetActive(true);
+            _cannonLvl2.gameObject.SetActive(false);
+            _cannonLvl3.gameObject.SetActive(false);
+            _currentCannon = _cannonLvl1;
+         }       
+         if (Input.GetKey(KeyCode.Alpha2))
+         {
+            _cannonLvl2.gameObject.SetActive(true);
+            _cannonLvl1.gameObject.SetActive(false);
+            _cannonLvl3.gameObject.SetActive(false);
+            _currentCannon = _cannonLvl2;
+         }
+         if (Input.GetKey(KeyCode.Alpha3))
+         {
+            _cannonLvl1.gameObject.SetActive(false);
+            _cannonLvl2.gameObject.SetActive(false);
+            _cannonLvl3.gameObject.SetActive(true);
+            _currentCannon = _cannonLvl3;
+         }
+    }
+}
